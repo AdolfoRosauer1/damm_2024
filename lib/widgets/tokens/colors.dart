324aa09a70f777819da8b869b36d44a062640dd0
adol <jurosauer@itba.edu.ar>
2024-03-27 16:37:27 -0300

    first tokens

---
+++ lib/widgets/tokens/colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+class ProjectPallette{
+  static Color neutral1 = const Color(0xFFFFFFFF);
+  static Color neutral2 = const Color(0x00191919);
+  static const neutral3 = Color(0xFAFAFA);
+  static const neutral4 = Color(0xE0E0E0);
+  static const neutral5 = Color(0x9E9E9E);
+  static const neutral6 = Color(0x666666);
+
+  static const primary1 = Color(0x0014903F);
+  static const primary2 = Color(0x00e7f4ec);
+  static const primary3 = Color(0x00f3f9f5);
+
+  static const secondary1 = Color(0xE1ECF5);
+  static const secondary2 = Color(0xCAE5FB);
+  static const secondary3 = Color(0x90CAF9);
+  static const secondary4 = Color(0x6CB6F3);
+  static const secondary5 = Color(0x2C98F0);
+  static const secondary6 = Color(0x0D47A1);
+
+  static const error = Color(0xB3261E);
+
+}
+
---
+++ lib/widgets/tokens/fonts.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+import 'colors.dart';
+
+class ProjectFonts{
+  static TextStyle headline1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 24.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle headline2 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 20.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle subtitle1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle body1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 14.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle body2 = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 12.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle button = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 14.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle caption = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w400,
+    fontSize: 12.0,
+    color: ProjectPallette.neutral2,
+  );
+
+  static TextStyle overline = TextStyle(
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w500,
+    fontSize: 10.0,
+    color: ProjectPallette.neutral2,
+    letterSpacing: 1.5,
+    height: 1.6
+  );
+}
