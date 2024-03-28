abd472bd7b90fe3bdc003eb23aa1c030807b3f03
Santiago <smonjeau@itba.edu.ar>
2024-03-28 20:45:31 -0300

    add icons atom

---
+++ lib/widgets/atoms/icons.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import '../tokens/colors.dart';
+
+class ProjectIcons{
+  //TODO iconos en 2 tonos, no se como ponerlos. También el de la foto de perfil.
+  //El size default ya es 24px
+  static Icon visibilityFilledEnabled = const Icon(Icons.visibility, color: ProjectPalette.neutral6);
+  static Icon visibilityFilledDisabled = const Icon(Icons.visibility, color: ProjectPalette.neutral4);
+  static Icon visibilityFilledActivated = const Icon(Icons.visibility, color: ProjectPalette.primary1);
+
+  static Icon visibilityOutlinedEnabled = const Icon(Icons.visibility_outlined, color: ProjectPalette.neutral6);
+  static Icon visibilityOutlinedDisabled = const Icon(Icons.visibility_outlined, color: ProjectPalette.neutral4);
+  static Icon visibilityOutlinedActivated = const Icon(Icons.visibility_outlined, color: ProjectPalette.primary1);
+
+  static Icon visibilityOffFilledEnabled = const Icon(Icons.visibility_off, color: ProjectPalette.neutral6);
+  static Icon visibilityOffFilledDisabled = const Icon(Icons.visibility_off, color: ProjectPalette.neutral4);
+  static Icon visibilityOffFilledActivated = const Icon(Icons.visibility_off, color: ProjectPalette.primary1);
+
+  static Icon visibilityOffOutlinedEnabled = const Icon(Icons.visibility_off_outlined, color: ProjectPalette.neutral6);
+  static Icon visibilityOffOutlinedDisabled = const Icon(Icons.visibility_off_outlined, color: ProjectPalette.neutral4);
+  static Icon visibilityOffOutlinedActivated = const Icon(Icons.visibility_off_outlined, color: ProjectPalette.primary1);
+
+  static Icon favoriteFilledEnabled = const Icon(Icons.favorite, color: ProjectPalette.neutral6);
+  static Icon favoriteFilledDisabled = const Icon(Icons.favorite, color: ProjectPalette.neutral4);
+  static Icon favoriteFilledActivated = const Icon(Icons.favorite, color: ProjectPalette.primary1);
+
+  static Icon favoriteOutlinedEnabled = const Icon(Icons.favorite_border, color: ProjectPalette.neutral6);
+  static Icon favoriteOutlinedDisabled = const Icon(Icons.favorite_border, color: ProjectPalette.neutral4);
+  static Icon favoriteOutlinedActivated = const Icon(Icons.favorite_border, color: ProjectPalette.primary1);
+
+  static Icon locationFilledEnabled = const Icon(Icons.location_on, color: ProjectPalette.neutral6);
+  static Icon locationFilledDisabled = const Icon(Icons.location_on, color: ProjectPalette.neutral4);
+  static Icon locationFilledActivated = const Icon(Icons.location_on, color: ProjectPalette.primary1);
+
+  static Icon locationOutlinedEnabled = const Icon(Icons.location_on_outlined, color: ProjectPalette.neutral6);
+  static Icon locationOutlinedDisabled = const Icon(Icons.location_on_outlined, color: ProjectPalette.neutral4);
+  static Icon locationOutlinedActivated = const Icon(Icons.location_on_outlined, color: ProjectPalette.primary1);
+
+  static Icon searchFilledEnabled = const Icon(Icons.search, color: ProjectPalette.neutral6);
+  static Icon searchFilledDisabled = const Icon(Icons.search, color: ProjectPalette.neutral4);
+  static Icon searchFilledActivated = const Icon(Icons.search, color: ProjectPalette.primary1);
+
+  static Icon addFilledEnabled = const Icon(Icons.add, color: ProjectPalette.neutral6);
+  static Icon addFilledDisabled = const Icon(Icons.add, color: ProjectPalette.neutral4);
+  static Icon addFilledActivated = const Icon(Icons.add, color: ProjectPalette.primary1);
+
+  static Icon backFilledEnabled = const Icon(Icons.arrow_back, color: ProjectPalette.neutral6);
+  static Icon backFilledDisabled = const Icon(Icons.arrow_back, color: ProjectPalette.neutral4);
+  static Icon backFilledActivated = const Icon(Icons.arrow_back, color: ProjectPalette.primary1);
+
+  static Icon calendarFilledEnabled = const Icon(Icons.calendar_month, color: ProjectPalette.neutral6);
+  static Icon calendarFilledDisabled = const Icon(Icons.calendar_month, color: ProjectPalette.neutral4);
+  static Icon calendarFilledActivated = const Icon(Icons.calendar_month, color: ProjectPalette.primary1);
+
+  static Icon errorFilledEnabled = const Icon(Icons.error, color: ProjectPalette.neutral6);
+  static Icon errorFilledDisabled = const Icon(Icons.error, color: ProjectPalette.neutral4);
+  static Icon errorFilledActivated = const Icon(Icons.error, color: ProjectPalette.error);
+
+  static Icon errorOutlineFilledEnabled = const Icon(Icons.error_outline, color: ProjectPalette.neutral6);
+  static Icon errorOutlineFilledDisabled = const Icon(Icons.error_outline, color: ProjectPalette.neutral4);
+  static Icon errorOutlineFilledActivated = const Icon(Icons.error_outline, color: ProjectPalette.error);
+
+  static Icon listFilledEnabled = const Icon(Icons.list, color: ProjectPalette.neutral6);
+  static Icon listFilledDisabled = const Icon(Icons.list, color: ProjectPalette.neutral4);
+  static Icon listFilledActivated = const Icon(Icons.list, color: ProjectPalette.primary1);
+  
+  static Icon mapFilledEnabled = const Icon(Icons.map, color: ProjectPalette.neutral6);
+  static Icon mapFilledDisabled = const Icon(Icons.map, color: ProjectPalette.neutral4);
+  static Icon mapFilledActivated = const Icon(Icons.map, color: ProjectPalette.primary1);
+
+  static Icon nearMeFilledEnabled = const Icon(Icons.near_me, color: ProjectPalette.neutral6);
+  static Icon nearMeFilledDisabled = const Icon(Icons.near_me, color: ProjectPalette.neutral4);
+  static Icon nearMeFilledActivated = const Icon(Icons.near_me, color: ProjectPalette.primary1);
+  
+  static Icon personFilledEnabled = const Icon(Icons.person, color: ProjectPalette.neutral6);
+  static Icon personFilledDisabled = const Icon(Icons.person, color: ProjectPalette.secondary4);
+  static Icon personFilledActivated = const Icon(Icons.person, color: ProjectPalette.secondary6);
+
+  static Icon radioButtonPressedEnabled = const Icon(Icons.radio_button_checked, color: ProjectPalette.neutral6);
+  static Icon radioButtonPressedDisabled = const Icon(Icons.radio_button_checked, color: ProjectPalette.neutral4);
+  static Icon radioButtonPressedActivated = const Icon(Icons.radio_button_checked, color: ProjectPalette.primary1);
+  static Icon radioButtonUnpressedEnabled = const Icon(Icons.radio_button_unchecked, color: ProjectPalette.neutral6);
+  static Icon radioButtonUnpressedDisabled = const Icon(Icons.radio_button_unchecked, color: ProjectPalette.neutral4);
+  static Icon radioButtonUnpressedActivated = const Icon(Icons.radio_button_unchecked, color: ProjectPalette.primary1);
+
+
+  static Icon profilePicture = const Icon(Icons.account_circle, color: ProjectPalette.secondary4, size:120);
+}
