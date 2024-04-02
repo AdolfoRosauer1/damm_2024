f96e936d268802bdb9ca342705371258580dcf8d
Santiago <smonjeau@itba.edu.ar>
2024-04-01 22:13:03 -0300

    start with news card

--- lib/widgets/cells/cards/volunteering_card.dart
+++ lib/widgets/cells/cards/volunteering_card.dart
@@ -3,11 +3,10 @@ import 'package:damm_2024/widgets/molecules/components/vacancies_chip.dart';
 import 'package:damm_2024/widgets/tokens/colors.dart';
 import 'package:damm_2024/widgets/tokens/fonts.dart';
 import 'package:damm_2024/widgets/tokens/shadows.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class VolunteeringCard extends StatelessWidget {
-  const VolunteeringCard({Key? key}) : super(key: key);
+  const VolunteeringCard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -46,11 +45,11 @@ class VolunteeringCard extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      VacanciesChip(vacancies: 20, enabled: true),
+                      const VacanciesChip(vacancies: 20, enabled: true),
                       Row(
                         children: [
                           ProjectIcons.favoriteOutlinedActivated,
-                          SizedBox(
+                          const SizedBox(
                             width: 16,
                           ),
                           ProjectIcons.locationFilledActivated,
