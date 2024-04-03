a1755e73f5b97708b49fc0bcda458d3ac630aefa
adol <jurosauer@itba.edu.ar>
2024-04-03 14:51:15 -0300

    changed voluntary card image to asset

--- lib/widgets/cells/cards/volunteering_card.dart
+++ lib/widgets/cells/cards/volunteering_card.dart
@@ -12,19 +12,18 @@ class VolunteeringCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(2),
-        boxShadow: ProjectShadows.shadow2
-      ),
+          borderRadius: BorderRadius.circular(2),
+          boxShadow: ProjectShadows.shadow2),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.network(
+          Image.asset(
+            'lib/resources/voluntary_card.png',
             height: 138,
-            'https://s3-alpha-sig.figma.com/img/6160/48a8/56fafc1f797d16aeaaa7f76477bdc239?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nSrnZy3veXu1utLO0iEVTj3ZELjEvZr3ow8oqiqILdyL7g2V8yWu7Hz9mcNqa8ynoicuAntXeek4j0hGITPYyAKUpUKldX4p5i-mTWSLvKYiHYHiD7BWwT5FVrTgUa9~TlM2yifBmbti~vyfixF37JwB0wGsvAghX2PYBqM46-ZmGBcq2ABlnJOILqtqskQS~RKxLO0gHMqiyOLxnkM37GePSydJX8FfDBUf6XJtrYqe4zLIS1R6Q1-rQNXa8VGrhxjEXTX393NrfbG1BTcknEMcXYZtk5nS7zEO8JNf~RyBZ5Z0cvKke0F9-aqKMjTxuYxIypougWGiGsQw~OAcyQ__', // URL de la imagen
             fit: BoxFit.cover,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Container(
               color: Colors.white,
               child: Column(
@@ -32,15 +31,13 @@ class VolunteeringCard extends StatelessWidget {
                 children: [
                   Text(
                     'ACCIÓN SOCIAL',
-                    style: ProjectFonts.overline.copyWith(
-                      color: ProjectPalette.neutral6
-                    ),
+                    style: ProjectFonts.overline
+                        .copyWith(color: ProjectPalette.neutral6),
                   ),
                   Text(
                     'Un Techo para mi País',
-                    style: ProjectFonts.subtitle1.copyWith(
-                      color: ProjectPalette.neutral2
-                    ),
+                    style: ProjectFonts.subtitle1
+                        .copyWith(color: ProjectPalette.neutral2),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
