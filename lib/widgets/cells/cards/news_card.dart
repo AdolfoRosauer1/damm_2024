fcf2d357fa4736f9245a7a7a7b87d2de8d1bf03c
Santiago <smonjeau@itba.edu.ar>
2024-04-03 21:57:38 -0300

    finished with news card

--- lib/widgets/cells/cards/news_card.dart
+++ lib/widgets/cells/cards/news_card.dart
@@ -1,129 +1,55 @@
-
 import 'package:damm_2024/widgets/tokens/colors.dart';
 import 'package:damm_2024/widgets/tokens/fonts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class NewsCard extends StatelessWidget{
+class NewsCard extends StatelessWidget {
   final String media;
   final String imageUrl;
   final String title;
   final String description;
-  const NewsCard({super.key, required this.media, required this.imageUrl, required this.title, required this.description});
+  const NewsCard(
+      {super.key,
+      required this.media,
+      required this.imageUrl,
+      required this.title,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      decoration: BoxDecoration(
-      border: Border.all(color: Colors.black), // Black border
-    ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Image.network(
-            imageUrl,
-            width: 118,
-            fit: BoxFit.cover,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    media,
-                    style: ProjectFonts.overline.copyWith(
-                      color: ProjectPalette.neutral6
-                    ),
-                  ),
-                  Text(
-                    title,
-                    style: ProjectFonts.subtitle1,
-                  ),
-                  Text(
-                    description,
-                    style: ProjectFonts.body2,
-                  ),
-                  SizedBox(height: 8),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black), // Black border
-                    ),
-                    child: Row(
-
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            print('Ver más');
-                          },
-                          style: TextButton.styleFrom(
-                            textStyle: ProjectFonts.button,
-                            foregroundColor: ProjectPalette.primary1,
-                          ),
-                          child: const Text('Leer Más'),
-                        ),
-                       // SizedBox(width: 4,)
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            
-            
-            ),
-          )
-          
-      
-      
-        ],
-      ),
-    );
-/*
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Image.network(
-              imageUrl,
-              fit: BoxFit.cover,
-              width: 118,
-            ),
-          ],
+        Image.network(
+          imageUrl,
+          width: 118,
+          fit: BoxFit.cover,
         ),
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Text(
-                  'REPORTE 2820',
+                  media,
                   style: ProjectFonts.overline.copyWith(
                     color: ProjectPalette.neutral6,
                   ),
                 ),
-                SizedBox(height: 4),
                 Text(
-                  'Ser donante voluntario',
+                  title,
                   style: ProjectFonts.subtitle1,
                 ),
-                SizedBox(height: 8),
                 Text(
-                  'Desde el Hospital Centenario recalcan la importancia de la donación voluntaria de Sangre',
+                  description,
                   style: ProjectFonts.body2,
                 ),
-                Spacer(),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
+                const SizedBox(height: 8),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomRight,
                     child: TextButton(
                       onPressed: () {
                         print('Ver más');
@@ -142,6 +68,5 @@ class NewsCard extends StatelessWidget{
         ),
       ],
     );
-    */
   }
 }
