fb4c2c6d9ea01456c6107f4677ab79e9d1e05ea9
LUCAS BIOLLEY <86691448+DJ4S@users.noreply.github.com>
2024-06-01 03:56:44 -0300

    Update news_details_screen.dart

--- lib/screens/news_details_screen.dart
+++ lib/screens/news_details_screen.dart
@@ -5,6 +5,7 @@ import 'package:damm_2024/widgets/molecules/buttons/cta_button.dart';
 import 'package:damm_2024/widgets/tokens/colors.dart';
 import 'package:damm_2024/widgets/tokens/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class NewsDetailsScreen extends StatefulWidget {
   const NewsDetailsScreen({Key? key, required this.id}) : super(key: key);
