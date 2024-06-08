86f83dfc361223ddc7c4e46a89dea690b24ba9a7
adol <jurosauer@itba.edu.ar>
2024-06-08 17:20:59 -0300

    Small riverpod reform + profile provider

--- lib/providers/auth_provider.g.dart
+++ lib/providers/auth_provider.g.dart
@@ -6,11 +6,26 @@ part of 'auth_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$firebaseAuthHash() => r'de3a3ba9417e77d3b1a07e739744ca3eeb662690';
+String _$authRepositoryHash() => r'd67e36f4529ddfb5b280eb37501bb59943123dae';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider = Provider<AuthRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthRepositoryRef = ProviderRef<AuthRepository>;
+String _$firebaseAuthHash() => r'46c40b7c5cf8ab936c0daa96a6af106bd2ae5d51';
 
 /// See also [firebaseAuth].
 @ProviderFor(firebaseAuth)
-final firebaseAuthProvider = Provider<AuthRepository>.internal(
+final firebaseAuthProvider = Provider<FirebaseAuth>.internal(
   firebaseAuth,
   name: r'firebaseAuthProvider',
   debugGetCreateSourceHash:
@@ -19,6 +34,21 @@ final firebaseAuthProvider = Provider<AuthRepository>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef FirebaseAuthRef = ProviderRef<AuthRepository>;
+typedef FirebaseAuthRef = ProviderRef<FirebaseAuth>;
+String _$authControllerHash() => r'df35f83a8543131791bb373c41af306773bcdbcb';
+
+/// See also [authController].
+@ProviderFor(authController)
+final authControllerProvider = Provider<AuthController>.internal(
+  authController,
+  name: r'authControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthControllerRef = ProviderRef<AuthController>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/providers/volunteer_provider.g.dart
+++ lib/providers/volunteer_provider.g.dart
@@ -6,19 +6,79 @@ part of 'volunteer_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$volunteerHash() => r'e0a346d0f2901ec6b1b7a548aea840f073c8e921';
+String _$firebaseFirestoreHash() => r'2e7f8bd195d91c027c5155f34b719187867bc113';
 
-/// See also [volunteer].
-@ProviderFor(volunteer)
-final volunteerProvider = AutoDisposeProvider<Volunteer>.internal(
-  volunteer,
-  name: r'volunteerProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$volunteerHash,
+/// See also [firebaseFirestore].
+@ProviderFor(firebaseFirestore)
+final firebaseFirestoreProvider = Provider<FirebaseFirestore>.internal(
+  firebaseFirestore,
+  name: r'firebaseFirestoreProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseFirestoreHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef VolunteerRef = AutoDisposeProviderRef<Volunteer>;
+typedef FirebaseFirestoreRef = ProviderRef<FirebaseFirestore>;
+String _$firebaseStorageHash() => r'ddec157566e3f96dac39d44de2cd99f9d71f9b54';
+
+/// See also [firebaseStorage].
+@ProviderFor(firebaseStorage)
+final firebaseStorageProvider = Provider<FirebaseStorage>.internal(
+  firebaseStorage,
+  name: r'firebaseStorageProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseStorageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseStorageRef = ProviderRef<FirebaseStorage>;
+String _$profileRepositoryHash() => r'd8d96cb97db0c026fbcfa56e65c39ca7c265e894';
+
+/// See also [profileRepository].
+@ProviderFor(profileRepository)
+final profileRepositoryProvider = Provider<ProfileRepository>.internal(
+  profileRepository,
+  name: r'profileRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$profileRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProfileRepositoryRef = ProviderRef<ProfileRepository>;
+String _$profileControllerHash() => r'6fa7e38ad8c6d6ff2e8b27f906fb5805f3c9f2a1';
+
+/// See also [profileController].
+@ProviderFor(profileController)
+final profileControllerProvider = Provider<ProfileController>.internal(
+  profileController,
+  name: r'profileControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$profileControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProfileControllerRef = ProviderRef<ProfileController>;
+String _$currentUserHash() => r'5e3cc4d02ea38d8f22626490d21f9922fe3c5f84';
+
+/// See also [CurrentUser].
+@ProviderFor(CurrentUser)
+final currentUserProvider = NotifierProvider<CurrentUser, Volunteer>.internal(
+  CurrentUser.new,
+  name: r'currentUserProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$currentUserHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentUser = Notifier<Volunteer>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
