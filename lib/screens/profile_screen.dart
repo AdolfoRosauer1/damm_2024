021856a54ec6d656f9f365a166530b76be5f5e79
adol <jurosauer@itba.edu.ar>
2024-06-08 17:31:22 -0300

    small fix again

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,9 +1,8 @@
 //TODO conectar con riverpod y statemanagement
 import 'package:damm_2024/models/gender.dart';
-import 'package:damm_2024/models/volunteer.dart';
+import 'package:damm_2024/providers/volunteer_provider.dart';
 import 'package:damm_2024/widgets/atoms/icons.dart';
 import 'package:damm_2024/widgets/cells/cards/information_card.dart';
-import 'package:damm_2024/widgets/cells/cards/profile_picture_card.dart';
 import 'package:damm_2024/widgets/cells/forms/personal_data_form.dart';
 import 'package:damm_2024/widgets/cells/modals/session_modal.dart';
 import 'package:damm_2024/widgets/molecules/buttons/cta_button.dart';
@@ -12,24 +11,20 @@ import 'package:damm_2024/widgets/molecules/components/profile_picture.dart';
 import 'package:damm_2024/widgets/tokens/colors.dart';
 import 'package:damm_2024/widgets/tokens/fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
-import 'package:damm_2024/providers/volunteer_provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-
 
 class ProfileScreen extends ConsumerStatefulWidget {
   static const route = "/profileScreen";
 
   const ProfileScreen({super.key});
-  
+
   @override
   ConsumerState<ConsumerStatefulWidget> createState() {
     return _ProfileScreenState();
   }
-
 }
 
 class _ProfileScreenState extends ConsumerState<ProfileScreen> {
@@ -37,123 +32,124 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final volunteer = ref.watch(volunteerProvider);
-    if (!volunteer.hasCompletedProfile()){
+    final volunteer = ref.watch(currentUserProvider);
+    if (!volunteer.hasCompletedProfile()) {
       return SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: 147.5,),
+              const SizedBox(
+                height: 147.5,
+              ),
               ProjectIcons.profilePicture,
-              const SizedBox(height: 16,),
-              Text(
-                AppLocalizations.of(context)!.volunteer,
-                style: ProjectFonts.overline.copyWith(color: ProjectPalette.neutral6)
+              const SizedBox(
+                height: 16,
               ),
-              const SizedBox(height: 8,),
-              Text(
-                '${volunteer.firstName} ${volunteer.lastName}',
-                style: ProjectFonts.subtitle1
+              Text(AppLocalizations.of(context)!.volunteer,
+                  style: ProjectFonts.overline
+                      .copyWith(color: ProjectPalette.neutral6)),
+              const SizedBox(
+                height: 8,
               ),
-              const SizedBox(height: 8,),
+              Text('${volunteer.firstName} ${volunteer.lastName}',
+                  style: ProjectFonts.subtitle1),
+              const SizedBox(
+                height: 8,
+              ),
               Text(
                 AppLocalizations.of(context)!.completeProfile,
-                style: ProjectFonts.body1.copyWith(color: ProjectPalette.neutral6),
-                textAlign: TextAlign.center,       
+                style:
+                    ProjectFonts.body1.copyWith(color: ProjectPalette.neutral6),
+                textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 147.5,),
+              const SizedBox(
+                height: 147.5,
+              ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 80),
                 child: ShortButton(
-                  onPressed: () => {
-                    context.go(PersonalDataForm.completeRoute)
-                  },
-                  buttonText:  AppLocalizations.of(context)!.complete,
+                  onPressed: () => {context.go(PersonalDataForm.completeRoute)},
+                  buttonText: AppLocalizations.of(context)!.complete,
                   activated: true,
-                  icon: Icon(ProjectIcons.addFilledEnabled.icon, color: ProjectPalette.neutral1),),
+                  icon: Icon(ProjectIcons.addFilledEnabled.icon,
+                      color: ProjectPalette.neutral1),
+                ),
               ),
             ],
           ),
         ),
       );
-    }else{
+    } else {
       return SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 32),
-              child: ProfilePicture(
-                imageUrl: volunteer.profileImageURL,
-                size: ProfilePictureSize.large,
-              ),
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: ProfilePicture(
+              imageUrl: volunteer.profileImageURL,
+              size: ProfilePictureSize.large,
             ),
-            const SizedBox(height: 16),
-            Text(
-              AppLocalizations.of(context)!.volunteer,
-              style: ProjectFonts.overline.copyWith(color: ProjectPalette.neutral6)
-            ),
-            const SizedBox(height: 2),
-            Text(
-              '${volunteer.firstName} ${volunteer.lastName}',
-              style: ProjectFonts.subtitle1
-            ), 
-            const SizedBox(height: 2),
-            Text(
-              volunteer.email,
-              style: ProjectFonts.body1
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
-              child: InformationCard(
+          ),
+          const SizedBox(height: 16),
+          Text(AppLocalizations.of(context)!.volunteer,
+              style: ProjectFonts.overline
+                  .copyWith(color: ProjectPalette.neutral6)),
+          const SizedBox(height: 2),
+          Text('${volunteer.firstName} ${volunteer.lastName}',
+              style: ProjectFonts.subtitle1),
+          const SizedBox(height: 2),
+          Text(volunteer.email, style: ProjectFonts.body1),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
+            child: InformationCard(
                 title: AppLocalizations.of(context)!.personalInformation,
                 label1: AppLocalizations.of(context)!.birthDate,
                 content1: dateFormat.format(volunteer.dateOfBirth!),
                 label2: AppLocalizations.of(context)!.gender,
                 content2: (volunteer.gender!.value)),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32,horizontal: 16),
-              child: InformationCard(
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+            child: InformationCard(
                 title: AppLocalizations.of(context)!.contactInformation,
                 label1: AppLocalizations.of(context)!.phone,
                 content1: volunteer.phoneNumber,
                 label2: 'E-MAIL',
                 content2: volunteer.email),
-            ),  
-            Container(
-              margin: const EdgeInsets.fromLTRB(26,0,26,46),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  CtaButton(
-                    enabled: true, 
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(26, 0, 26, 46),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CtaButton(
+                  enabled: true,
+                  onPressed: () => {context.go(PersonalDataForm.completeRoute)},
+                  filled: true,
+                  actionStr: AppLocalizations.of(context)!.editProfile,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                CtaButton(
+                    enabled: true,
                     onPressed: () => {
-                      context.go(PersonalDataForm.completeRoute)
-                    }, 
-                    filled: true, 
-                    actionStr: AppLocalizations.of(context)!.editProfile,
-                  ),
-                  const SizedBox(height: 8,),
-                  CtaButton(enabled: true, onPressed: () => {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return const SessionModal();
-                      }
-                    )
-                  }, filled: false, actionStr: AppLocalizations.of(context)!.logout, textColor: ProjectPalette.error),
-                ],
-              ),
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return const SessionModal();
+                              })
+                        },
+                    filled: false,
+                    actionStr: AppLocalizations.of(context)!.logout,
+                    textColor: ProjectPalette.error),
+              ],
             ),
-          ],
-        )
-      
-      );
+          ),
+        ],
+      ));
     }
-
   }
-  
-  
 }
