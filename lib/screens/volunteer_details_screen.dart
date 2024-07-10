769e5ed5eae2fdc3cfbdcf5cc469f2570bacf818
Santiago <smonjeau@itba.edu.ar>
2024-07-10 19:22:18 -0300

    fix import

--- lib/screens/volunteer_details_screen.dart
+++ lib/screens/volunteer_details_screen.dart
@@ -18,6 +18,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class VolunteerDetailsScreen extends ConsumerStatefulWidget {
